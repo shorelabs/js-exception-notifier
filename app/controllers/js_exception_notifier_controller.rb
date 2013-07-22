c6cb006511784d0ebaaaa66c2dfc7fca57c6a911
blazej-shorelabs <b.hadzik@shorelabs.com>
2013-07-22 11:41:28 +0200

    Throws exception for some who passed dependency of exception_notification gem but tries to run on different environment.

--- app/controllers/js_exception_notifier_controller.rb
+++ app/controllers/js_exception_notifier_controller.rb
@@ -1,4 +1,7 @@
 class JsExceptionNotifierController < ApplicationController
+
+  before_filter :exception_notification_gem_available?
+
   class JSException < StandardError
     attr_reader :message
     def initialize(message)
@@ -11,4 +14,14 @@ class JsExceptionNotifierController < ApplicationController
     ExceptionNotifier.notify_exception(JSException.new(params['errorMsg'].to_s), :data => {:error => params['errorMsg'], :file=> params['file'], :line=> params['lineNumber'], :browser=> params['browserInfo']})
     render :nothing=> true
   end
+
+  # Throws exception for some who passed dependency of exception_notification gem but tries to run on different environment.
+  def exception_notification_gem_available?
+    begin
+      Module.const_get('exception_notification')
+    rescue NameError => e
+      raise("Load exception_notification gem for #{Rails.env.to_s} first!")
+    end
+  end
+
 end
